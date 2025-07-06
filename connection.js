890149a2fd4485b6aa5a1c6cb930bab05a24cd8c
Gaurav Phule <gaurav230605@gmail.com>
2025-07-06 20:53:15 +0530

    mongo

--- connection.js
+++ connection.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
+    console.log(process.env.MONGO_URI)
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
