c061d393c1122743fe2cc08ae6592bed6f368132
Gaurav Phule <gaurav230605@gmail.com>
2025-07-06 20:44:08 +0530

    vercel tmp folder 2

--- Middlewares/multer.middleware.js
+++ Middlewares/multer.middleware.js
@@ -2,12 +2,12 @@ const multer = require("multer")
 const path = require("path");
 
 const maxSize = 3 * 1024 * 1024; // 3MB (7,340,032 bytes)
-const uploadDir = path.join(__dirname, "/tmp");
+// const uploadDir = path.join(__dirname,"/tmp");
 
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null,uploadDir)
+    cb(null,"/tmp")
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname)
